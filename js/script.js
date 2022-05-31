dfc4476c5745d1b42973c0d32ed140b3fae20b7e
omar96developer <omakino@hotmail.it>
2022-05-31 17:12:46 +0200

    created function getAllNotes whitch calls to mockapi and getall the notes

--- js/script.js
+++ js/script.js
@@ -3,4 +3,26 @@ $(document).ready(function () {
     //global daysjs locale it and utc plugin
     dayjs.locale('it');
     dayjs.extend(window.dayjs_plugin_utc);
+
+    getAllNotes();
 });
+
+// [1] api Ajax
+function getAllNotes() {
+    $.ajax({
+        method: 'GET',
+        url: 'https://62961666810c00c1cb6ed9b8.mockapi.io/notes?sortBy=updatedAt&order=desc',
+        success: function(data) {
+            if(data.length > 0){
+                printListNotes(data);
+            }
+        },
+        error: function (err){
+            console.log(err);
+        }
+    });
+}
+
+function printListNotes(data) {
+    console.log(data);
+}
